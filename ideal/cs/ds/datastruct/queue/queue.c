109ccf6d90b7f67a90b4165e564cc62f3fdaa6a1
ykjver <ykjver@gmail.com>
2018-09-18 18:34:43 +0800

    add queue

---
+++ ideal/cs/ds/datastruct/queue/queue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Queue {
+    int front, rear, size;
+    int capacity;
+    int *data;
+};
+
+
+struct Queue* createQueue(int item) {
+    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+    queue
+}
+
+int main() {
+
+
+}
